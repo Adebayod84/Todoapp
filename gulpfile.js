e6faa3a943ad94f618a7526d047a90087a888603
Adebayo Oduwobi <adebayooduwobi@gmail.com>
2020-12-08 17:16:25 -0600

    Adds concatenation, minification, sourcemaps to gulp JS tasks. Adds html and css tasks that move them to dist directory.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,30 @@
+const GulpClient = require('gulp');
 const {src, dest, series, parallel} = require('gulp');
 const minify = require('gulp-minify');
+const concat = require('gulp-concat');
 
 
 const scriptTask = () => {
-  return src('src/**/*.js')
-  .pipe(dest('dist/'));
+  return src('src/**/*.js', {sourcemaps: true})
+  .pipe(minify({
+    noSource: true,
+  }))
+  .pipe(concat("all.js"))
+  .pipe(dest('dist/js/', {sourcemaps: true}));
+
 }
 
-exports.scripts = series(scriptTask);
+
+const markupTask = () => {
+  return src('src/*.html')
+  .pipe(dest('dist/'))
+}
+
+
+const styleTask = () => {
+  return src('src/*.css')
+  .pipe(dest('dist/'))
+}
+
+
+exports.all = parallel(scriptTask, styleTask, markupTask);
